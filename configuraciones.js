0f9a321328ddd088a385da5cf77c814f4bfcc598
wild <wild.addsystem@gmail.com>
2024-01-12 17:27:45 -0400

    at
    act

---
+++ configuraciones.js
@@ -0,0 +1,10 @@
+// crear variables y ocupar en el index
+var mes = "Diciembre";
+var dia = "28";
+var año = 2024;
+var fechaDeEvento = '28·12·2024';
+// Accede a los elementos HTML por su id y asigna los valores
+document.getElementById("mes").innerText = mes;
+document.getElementById("dia").innerText = dia;
+document.getElementById("año").innerText = año;
+document.getElementById("fechaDeEvento").innerText = fechaDeEvento;
